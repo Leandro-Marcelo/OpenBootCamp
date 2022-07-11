f2381713f878ed6173cd033bc1c2781869d1101b
LeandroMarcelo <lmarcelos2019@gmail.com>
2022-07-11 18:14:08 -0400

    ultimo

--- client/src/components/OptionalRender.jsx
+++ client/src/components/OptionalRender.jsx
@@ -10,6 +10,7 @@ const LogoutButton = ({ logoutAction }) => {
 
 export default function OptionalRender() {
     const [access, setAccess] = useState(true);
+    const [nMessages, setnMessages] = useState(0);
 
     let optionalButton;
 
@@ -28,8 +29,32 @@ export default function OptionalRender() {
     if (access) {
         optionalButton = <LogoutButton logoutAction={logoutAction} />;
     } else {
-        optionalButton = <LogoutButton logginAction={logginAction} />;
+        optionalButton = <LoginButton logginAction={logginAction} />;
     }
 
-    return <div>{optionalButton}</div>;
+    // unread message
+    let addMessages = () => {
+        setnMessages(nMessages + 1);
+    };
+
+    return (
+        <div>
+            {/* Optional Button */}
+            {optionalButton}
+            {/* N message unread */}
+            {nMessages > 0 && nMessages === 1 && (
+                <p>You have {nMessages} unread message</p>
+            )}
+            {nMessages > 0 && <p>You have {nMessages} new messages...</p>}
+            {nMessages === 0 && <p>You have no new messages...</p>}
+            <button onClick={addMessages}>Add New Message</button>
+            {/* Ternary Operator */}
+            {nMessages > 0 && nMessages === 1 ? (
+                <p>You have {nMessages} new message </p>
+            ) : (
+                <p>You have no new messages...</p>
+            )}
+            <button onClick={addMessages}>Add your first message</button ? d>
+        </div>
+    );
 }
